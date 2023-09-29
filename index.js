eab516ca590e780031fc85389aa1f149d4a85da2
gachaupg <gachaupg607@gmail.com>
2023-09-28 23:27:26 -1200

    ggg

--- index.js
+++ index.js
@@ -28,12 +28,7 @@ const __dirname = path.resolve();
 
 const app = express();
 app.use(cors());
-app.use(cors(corsOptions));
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.use(express.json());
 

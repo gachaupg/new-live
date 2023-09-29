fdb4b6cea80a83fbc6d412a11c0789896f280e2b
gachaupg <gachaupg607@gmail.com>
2023-09-28 23:10:52 -1200

    jjj

--- client/src/pages/user/UserPodicast.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserPodicast = () => {
-  return (
-    <div>UserPodicast</div>
-  )
-}
-
-export default UserPodicast
--- client/src/pages/user/UserShows.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserShows = () => {
-  return (
-    <div>UserShows</div>
-  )
-}
-
-export default UserShows

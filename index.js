1d2d632bee11f4c0c335cf8a434bd4cfcd49e5ce
gachaupg <gachaupg607@gmail.com>
2023-09-28 23:32:31 -1200

    nbbb

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.get(('/'), (req,res) => {
+res.send('hello')});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
